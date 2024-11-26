e1878b9a4eba8addffe7a03cf8ea82f9761760c0
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-11-26 15:38:01 +0800

    implemented get and set methods for ctah

--- src/ciet_simulator_v1/app/panels_and_pages/ciet_data.rs
+++ src/ciet_simulator_v1/app/panels_and_pages/ciet_data.rs
@@ -4,6 +4,12 @@
 ///
 /// This is much easier compared to having arc mutex locks for each 
 /// piece of data. 
+///
+/// the right way to read CIETState is to obtain a lock, clone it, and 
+/// drop the lock 
+///
+/// the right way to write to CIETState is to have a clone of CIETState 
+/// ready, obtain a lock, then overwrite it completely
 #[derive(Debug,Clone,Copy)]
 pub struct CIETState {
     pub heater_power_kilowatts: f64,
@@ -19,6 +25,7 @@ pub struct CIETState {
     pub bt_21_dhx_tube_outlet_deg_c: f64,
     pub bt_65_tchx_inlet_deg_c: f64,
     pub bt_66_tchx_outlet_deg_c: f64,
+    pub bt_66_tchx_outlet_set_pt_deg_c: f64,
     pub fm_60_dracs_kg_per_s: f64,
     pub fm_20_dhx_branch_kg_per_s: f64,
     pub fm_40_ctah_branch_kg_per_s: f64,
@@ -40,6 +47,7 @@ impl Default for CIETState {
             bt_21_dhx_tube_outlet_deg_c: 21.0,
             bt_65_tchx_inlet_deg_c: 21.0,
             bt_66_tchx_outlet_deg_c: 21.0,
+            bt_66_tchx_outlet_set_pt_deg_c: 21.0,
             fm_60_dracs_kg_per_s: 0.0,
             fm_20_dhx_branch_kg_per_s: 0.0,
             fm_40_ctah_branch_kg_per_s: 0.0,
@@ -48,5 +56,71 @@ impl Default for CIETState {
 }
 
 impl CIETState {
+    /// takes another ciet_state object and overwrites it
+    pub fn overwrite_state(&mut self, ciet_state: Self){
+        *self = ciet_state;
+    }
+
+    /// reads heater power from the state 
+    pub fn get_heater_power_kilowatts(&self) -> f64 {
+        return self.heater_power_kilowatts;
+    }
+
+    /// heater
+    pub fn set_heater_power_kilowatts(&mut self, heater_power_kw: f64){
+        self.heater_power_kilowatts = heater_power_kw;
+    }
+
+    pub fn get_heater_outlet_temp_degc(&self) -> f64 {
+        return self.bt_12_heater_outlet_deg_c;
+    }
+
+    pub fn get_heater_inlet_temp_degc(&self) -> f64 {
+        return self.bt_11_heater_inlet_deg_c;
+    }
+
+    /// dhx methods
+    pub fn get_dhx_shell_outlet_temp_degc(&self) -> f64 {
+        return self.bt_25_dhx_shell_outlet_deg_c;
+    }
+
+    pub fn get_dhx_shell_inlet_temp_degc(&self) -> f64 {
+        return self.bt_21_dhx_shell_inlet_deg_c;
+    }
+
+    pub fn get_dhx_tube_outlet_temp_degc(&self) -> f64 {
+        return self.bt_21_dhx_tube_outlet_deg_c;
+    }
+
+    pub fn get_dhx_tube_inlet_temp_degc(&self) -> f64 {
+        return self.bt_60_dhx_tube_inlet_deg_c;
+    }
+
+    /// tchx methods
+    pub fn get_tchx_outlet_temp_degc(&self) -> f64 {
+        return self.bt_66_tchx_outlet_deg_c;
+    }
+
+    pub fn set_tchx_outlet_temp_degc(&mut self, tchx_out_degc: f64){
+        self.bt_66_tchx_outlet_set_pt_deg_c = tchx_out_degc;
+    }
+
+    pub fn get_tchx_inlet_temp_degc(&self) -> f64 {
+        return self.bt_65_tchx_inlet_deg_c;
+    }
+
+    /// ctah methods
+    pub fn get_ctah_outlet_temp_degc(&self) -> f64 {
+        return self.bt_41_ctah_outlet_deg_c;
+    }
+
+    pub fn set_ctah_outlet_temp_degc(&mut self, ctah_out_degc: f64){
+        self.bt_41_ctah_outlet_set_pt_deg_c = ctah_out_degc;
+    }
+
+    pub fn get_ctah_inlet_temp_degc(&self) -> f64 {
+        return self.bt_43_ctah_inlet_deg_c;
+    }
+
 }
 
