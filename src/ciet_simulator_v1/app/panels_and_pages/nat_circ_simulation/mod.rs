67e056d6437f8dd289c91a24faeb5c59ac8192f6
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-11-27 21:42:48 +0800

    added updates for hot branch

--- src/ciet_simulator_v1/app/panels_and_pages/nat_circ_simulation/mod.rs
+++ src/ciet_simulator_v1/app/panels_and_pages/nat_circ_simulation/mod.rs
@@ -666,6 +666,64 @@ pub fn coupled_dracs_loop_version_7(
             (bt_12.get::<degree_celsius>()*100.0)
             .round()/100.0;
 
+        // hot branch update 
+        {
+            let pipe_1a_temp = 
+                heater_top_head_1a
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_1a_temp_degc = 
+                pipe_1a_temp.get::<degree_celsius>() as f32;
+
+            let pipe_1b_temp = 
+                heater_bottom_head_1b
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_1b_temp_degc = 
+                pipe_1b_temp.get::<degree_celsius>() as f32;
+
+            let pipe_2a_temp = 
+                pipe_2a
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_2a_temp_degc = 
+                pipe_2a_temp.get::<degree_celsius>() as f32;
+
+            let pipe_2_temp = 
+                static_mixer_10_label_2
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_2_temp_degc = 
+                pipe_2_temp.get::<degree_celsius>() as f32;
+
+            let pipe_3_temp = 
+                pipe_3
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_3_temp_degc = 
+                pipe_3_temp.get::<degree_celsius>() as f32;
+
+            let pipe_4_temp = 
+                pipe_4
+                .pipe_fluid_array
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            local_ciet_state.pipe_4_temp_degc = 
+                pipe_4_temp.get::<degree_celsius>() as f32;
+
+        }
+
 
 
         current_simulation_time += timestep;
